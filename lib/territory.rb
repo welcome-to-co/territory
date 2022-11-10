14fc6afc17d659ca578f5c32afa52a5e7010430c
buffpojken <daniel@sykewarrior.com>
2022-11-10 15:38:09 +0100

    fixed missing files

--- lib/territory.rb
+++ lib/territory.rb
@@ -7,6 +7,7 @@ require 'territory/area'
 require 'territory/circle'
 require 'territory/destination'
 require 'territory/centroid'
+require "turf/invariant"
 require 'territory/sphericalmercator'
 require_relative "territory/version"
 
