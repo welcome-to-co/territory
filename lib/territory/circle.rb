114a725395a08b49c6cbd627268def8750aef1f3
buffpojken <daniel@sykewarrior.com>
2022-11-10 15:30:46 +0100

    added circle-calculation to Territory

---
+++ lib/territory/circle.rb
@@ -0,0 +1,18 @@
+module Territory
+
+  def self.circle(origin, radius, options = {})
+     steps       = options[:steps] || 64
+     properties  = options[:properties] ? options[:properties] : ((!origin.kind_of?(Array) && origin[:type] == "Feature" && origin[:properties]) ? origin[:properties] : {})
+     units       = options[:units] || 'kilometers'
+
+     coordinates = []
+     steps.times do |i|
+       coordinates.push(Territory.destination(origin, radius, (i * -360)/steps, {units: units})[:geometry][:coordinates])
+     end
+
+     coordinates.push(coordinates[0])
+
+     polygon([coordinates], properties)
+   end
+
+end
